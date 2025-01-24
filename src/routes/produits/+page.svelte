ca0faa2170e214489c115f9fc8da879d879beb05
yvan <y.amigoni@gmail.com>
2025-01-24 14:36:48 +0100

    add styling and header component

---
+++ src/routes/produits/+page.svelte
@@ -0,0 +1,230 @@
+<h1>Tous nos produits</h1>
+
+<div class="container flex row flx-wrap">
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+    
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+    
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+    
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img
+              src="https://bulma.io/assets/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <!-- svelte-ignore a11y_img_redundant_alt -->
+                <img
+                  src="https://bulma.io/assets/images/placeholders/96x96.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+            iaculis mauris. <a href="/">@bulmaio</a>. <a href="/">#css</a>
+            <!-- svelte-ignore a11y_invalid_attribute -->
+            <a href="#">#responsive</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+    </div>
+</div>
